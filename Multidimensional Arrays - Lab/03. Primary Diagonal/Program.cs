3e1733f9ca77c74913fbadd627c61835b60b27a8
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-05 19:52:19 +0100

    add task

---
+++ Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+            int sum = 0;
+           
+                for (int i = n-1; i >=0; i--)
+                {
+                    sum += matrix[i, i];
+                }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
