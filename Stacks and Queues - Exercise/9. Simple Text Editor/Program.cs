faf4b4be953a6693bcabd41fe696436d1b4aa07a
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-07 22:28:45 +0100

    add tasks

---
+++ Stacks and Queues - Exercise/9. Simple Text Editor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+          
+            Stack<string> changes = new Stack<string>();
+            string text = string.Empty;
+        
+            int operationsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < operationsCount; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                int command = int.Parse(tokens[0]);
+
+                switch (command)
+                {
+                    case 1:
+                        changes.Push(text);
+                        text += tokens[1];
+                        break;
+                    case 2:
+                        changes.Push(text);
+                        int count = int.Parse(tokens[1]);
+                        text = text.Remove(text.Length - count);
+                        break;
+                    case 3:
+                        int index = int.Parse(tokens[1]) - 1;
+                        Console.WriteLine(text[index]);
+                        break;
+                    case 4:
+                        text = changes.Pop();
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
