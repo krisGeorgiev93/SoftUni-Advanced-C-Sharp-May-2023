9ebae883741117833152c96e5e8c6d8eb01b85ec
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-07 22:28:01 +0100

    add tasks

---
+++ Stacks and Queues - Exercise/6. Songs Queue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ").ToArray();
+
+            Queue<string> queue = new Queue<string>(songs);
+            string command = string.Empty;
+            while (queue.Any())
+            {
+                command = Console.ReadLine();
+                string[] commandParts = command.Split();
+                string commandType = commandParts[0];
+
+                if (commandType == "Play")
+                {
+                    queue.Dequeue();
+                }
+                if (commandType == "Add")
+                {
+                    string song = string.Join(" ", commandParts.Skip(1));
+                    if (queue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        queue.Enqueue(song);
+                    }
+                }
+                if (commandType == "Show")
+                {
+                    Console.WriteLine(string.Join(", ",queue));
+                }
+
+
+            }
+            Console.WriteLine("No more songs!");
+
+        }
+    }
+}
