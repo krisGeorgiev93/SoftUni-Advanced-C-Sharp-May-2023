e0402d223d4b5aad1ec980788c40a180885bbebb
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-07 22:28:30 +0100

    add tasks

---
+++ Stacks and Queues - Exercise/8. Balanced Parentheses/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parentheses = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var parenthese in parentheses)
+            {
+                switch (parenthese)
+                {
+                    case '{':
+                    case '(':
+                    case '[':
+                        stack.Push(parenthese);
+                        break;
+                    case '}':
+                        if (stack.Count == 0 || stack.Pop() != '{')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case ')':
+                        if (stack.Count == 0 || stack.Pop() != '(')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case ']':
+                        if (stack.Count == 0 || stack.Pop() != '[')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                }
+            }
+
+            if (stack.Count > 0) // Missed case in judge ((((()))
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
