db9647a74a0bbd5402c8ad35420070f73456c739
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-07 22:26:57 +0100

    add tasks

---
+++ Stacks and Queues - Exercise/2. Basic Queue Operations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>();
+            int n = array[0];
+            int s = array[1];
+            int x = array[2];
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (queue.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+
+            }
+        }
+    }
+}
