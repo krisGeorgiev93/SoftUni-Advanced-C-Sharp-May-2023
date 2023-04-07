6ae66ee6f6ff5ef73f520b3038046837041fe432
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-07 22:27:45 +0100

    add tasks

---
+++ Stacks and Queues - Exercise/5. Fashion Boutique/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] boxes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int capacityOfRack = int.Parse(Console.ReadLine());
+            int totalRacks = 1;
+            int currentRack = capacityOfRack;
+            Stack<int> stack = new Stack<int>(boxes);
+
+            while (stack.Any())
+            {
+                if (currentRack - stack.Peek() >= 0)
+                {
+                    currentRack -= stack.Pop();                   
+                }
+                else
+                {
+                    totalRacks++;
+                    currentRack = capacityOfRack;
+                    currentRack -= stack.Pop();
+                }
+            }
+
+            Console.WriteLine(totalRacks);
+        }
+    }
+}
