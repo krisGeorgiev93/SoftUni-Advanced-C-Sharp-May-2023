634f3d79ae0b36e2c2194feb04809613215d278e
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-10 20:06:35 +0100

    add tasks

---
+++ Stacks and Queues - Lab/Queues - 6. Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Queues___6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            Queue<string> customers = new Queue<string>();
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (command != "Paid")
+                {
+                    customers.Enqueue(command);
+                }
+                else
+                {
+                    while (customers.Count > 0)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                    }
+}
+}
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
