fee9dfe472d3a5bddbee8f8e553b6982afb6e5da
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-13 20:06:42 +0100

    add tasks

---
+++ Sets and Dictionaries Advanced - Lab/3. Largest 3 Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] sortedNumbers = numbers.OrderByDescending(x => x).Take(3).ToArray();
+
+            Console.WriteLine(string.Join(" ",sortedNumbers));
+        }
+    }
+}
