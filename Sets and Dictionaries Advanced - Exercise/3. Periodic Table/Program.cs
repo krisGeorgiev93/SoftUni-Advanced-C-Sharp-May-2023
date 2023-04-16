e7bf22668d90d48b3288a6f77ba0e7b683881fff
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-16 22:48:31 +0100

    Add tasks

---
+++ Sets and Dictionaries Advanced - Exercise/3. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           SortedSet<string> uniqueElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split().ToArray();
+                foreach (var item in elements)
+                {
+                    uniqueElements.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",uniqueElements));
+        }
+    }
+}
