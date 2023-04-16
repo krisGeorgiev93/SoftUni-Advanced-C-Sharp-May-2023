0551ca9e126b2fe9b47a54141c02b64ca1983c7a
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-16 22:48:47 +0100

    Add tasks

---
+++ Sets and Dictionaries Advanced - Exercise/4. Even Times/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(number))
+                {
+                    numbers.Add(number, 0);
+                }
+
+                numbers[number]++;
+            }
+
+            Console.WriteLine(numbers.Single(n => n.Value % 2 == 0).Key);
+        }
+    }
+}
