ab42411ae768a63e29cd49e08aa70669f4a98283
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-17 20:58:54 +0100

    add tasks

---
+++ Sets and Dictionaries Advanced - Exercise/9. SoftUni Exam Results/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _9._SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            SortedDictionary<string, int> participantsPoints = new SortedDictionary<string, int>();
+            SortedDictionary<string, int> languagesSubmissions = new SortedDictionary<string, int>();
+
+            while ((input = Console.ReadLine()) != "exam finished")
+            {
+                string[] tokens = input.Split("-");
+                string name = tokens[0];
+                string language = tokens[1];
+                if (language == "banned")
+                {
+                    participantsPoints.Remove(name);
+                    continue;
+                }
+
+                int points = int.Parse(tokens[2]);
+                 
+                if (!participantsPoints.ContainsKey(name))
+                {
+                    participantsPoints.Add(name, 0);
+                }
+
+                if (participantsPoints[name] < points)
+                {
+                    participantsPoints[name] = points;
+                }
+
+
+                if (!languagesSubmissions.ContainsKey(language))
+                {
+                    languagesSubmissions.Add(language, 0);
+                }
+                languagesSubmissions[language]++;
+
+            }
+
+            Console.WriteLine("Results:");
+            foreach (var participant in participantsPoints.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{participant.Key} | {participant.Value}");
+            }
+            Console.WriteLine("Submissions:");
+            foreach (var submission in languagesSubmissions.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{submission.Key} - {submission.Value}");
+            }
+
+        }
+    }
+}
