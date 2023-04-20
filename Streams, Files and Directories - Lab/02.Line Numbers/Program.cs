307c82e657d6573d6db44f586998d7aa6dd4289c
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-20 14:37:07 +0100

    Add tasks

---
+++ Streams, Files and Directories - Lab/02.Line Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    string currentLine = reader.ReadLine();
+                    int row = 1;
+                    while (currentLine != null)
+                    {
+                        writer.WriteLine($"{row++}. {currentLine}");
+
+                        currentLine = reader.ReadLine();
+                    }
+                }
+            }
+
+        }
+    }
+}
