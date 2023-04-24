21c71010e886e9ffdcf0f0689c585de2687d450c
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-24 22:46:33 +0100

    add demo

---
+++ Recursion - Demo/StackOverFlowException/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StackOverFlowException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackOverflowSux();
+        }
+
+         static unsafe void StackOverflowSux()
+        {
+            int x = 5;
+            Console.WriteLine((int)&x);
+            StackOverflowSux();
+        }
+    }
+}
