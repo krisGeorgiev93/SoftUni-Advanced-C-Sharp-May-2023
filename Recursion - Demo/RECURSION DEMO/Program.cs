8be5201fe39011767647d2e4e40cd4966602e40b
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-24 22:46:05 +0100

    add demo

---
+++ Recursion - Demo/RECURSION DEMO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RECURSION_DEMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3 };
+
+            Console.WriteLine(Sum(array, 0)); 
+
+            static int Sum(int [] array, int index)
+            {
+                if (index == array.Length)
+                {
+                    return 0;
+                }
+
+                Console.WriteLine("Before entering recursive method");
+                int currentSum = array[index] + Sum(array, index + 1);
+                Console.WriteLine(currentSum);
+                Console.WriteLine("After entering recursive method");
+
+                return currentSum;
+
+            }
+        }
+    }
+}
