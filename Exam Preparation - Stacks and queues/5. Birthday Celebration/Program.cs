8cafe3fac3ac6cd83480654c355a28c8c2768f22
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-05-02 22:55:54 +0100

    Solutions

---
+++ Exam Preparation - Stacks and queues/5. Birthday Celebration/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCelebration
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var guests = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+            var meals = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+
+            int wastedfood = 0;
+
+            int currentGuest = guests.Peek();
+            int currentMeal = meals.Peek();
+
+            while (guests.Count > 0 && meals.Count > 0)
+            {
+                currentGuest -= currentMeal;
+                if (currentGuest > 0)
+                {
+                    meals.Pop();
+                    if (meals.Count == 0)
+                    {
+                        break;
+                    }
+                    currentMeal = meals.Peek();
+                }
+                else
+                {
+                    wastedfood += Math.Abs(currentGuest);
+                    guests.Dequeue();
+                    meals.Pop();
+                    if (guests.Count == 0 || meals.Count == 0)
+                    {
+                        break;
+                    }
+                    currentGuest = guests.Peek();
+                    currentMeal = meals.Peek();
+                }
+            }
+
+            if (meals.Count > 0)
+            {
+                Console.WriteLine($"Plates: {string.Join(' ', meals)}");
+            }
+            if (guests.Count > 0)
+            {
+                Console.WriteLine($"Guests: {string.Join(' ', guests)}");
+            }
+            Console.WriteLine($"Wasted grams of food: {wastedfood}");
+        }
+    }
+}
