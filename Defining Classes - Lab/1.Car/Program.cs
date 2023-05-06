6d9a19fe384bce1beb670d69320e5b03a66661bb
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-05-06 22:37:46 +0100

    Solutions

---
+++ Defining Classes - Lab/1.Car/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "GOLF";
+            car.Year = 1999;
+
+            Console.WriteLine($"{car.Make}\n{ car.Model}\n{car.Year}");
+        }
+    }
+}
