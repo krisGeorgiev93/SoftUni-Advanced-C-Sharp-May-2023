003d69c9b7b9fc974dc00ad5c39671d27f3cbd72
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-05-06 22:37:59 +0100

    Solutions

---
+++ Defining Classes - Lab/2.Car Extension/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Car_Extension
+{
+    public class Car
+    {
+        //fields
+        private string make;
+        private string model;
+        private int year;
+        private double fuelConsumption;
+        private double fuelQuantity;
+
+        //props
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model;} set { model = value; } }
+        public int Year { get { return year; } set { year = value; } }
+        public double FuelConsumption { get {return fuelConsumption; } set { fuelConsumption = value; } }
+        public double FuelQuantity { get { return fuelQuantity; } set { fuelQuantity = value; } }   
+
+        //Create void method
+       public void Drive(double distance)
+        {
+            double neededFuel = distance * fuelConsumption;
+
+            if (fuelQuantity - neededFuel > 0)
+            {
+                fuelQuantity -= neededFuel;
+            }
+
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        //Create string method
+        public string WhoIAm()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.AppendLine($"Fuel: {this.FuelQuantity:f2}");
+
+            return result.ToString();
+        }
+
+    }
+}
---
+++ Defining Classes - Lab/2.Car Extension/Program.cs
@@ -0,0 +1,25 @@
+﻿using _2.Car_Extension;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+            Car car = new Car();
+            car.Make = "BMW";
+            car.Model = "X3";
+            car.Year = 2008;
+
+            car.FuelQuantity = 100;
+            car.FuelConsumption = 2;
+
+            car.Drive(10);
+            car.Drive(1000);
+
+            Console.WriteLine(car.WhoIAm());
+
+        }
+    }
+}
