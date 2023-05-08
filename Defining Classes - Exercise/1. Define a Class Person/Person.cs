2fcba829730e01793121eb3fd818e31dd286b785
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-05-08 22:02:18 +0100

    Solutions

---
+++ Defining Classes - Exercise/1. Define a Class Person/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { age = value; }
+        }
+    }
+}
---
+++ Defining Classes - Exercise/1. Define a Class Person/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using DefiningClasses;
+public class StartUp
+{
+    private static void Main(string[] args)
+    {
+        Person peter = new Person();
+        peter.Name = "Peter";
+        peter.Age = 18;
+
+        Person george = new Person();
+        george.Name = "George";
+        george.Age = 20;
+
+        Console.WriteLine($"{peter.Name} is {peter.Age} years old");
+        Console.WriteLine($"{george.Name} is {george.Age} years old");
+    }
+}
