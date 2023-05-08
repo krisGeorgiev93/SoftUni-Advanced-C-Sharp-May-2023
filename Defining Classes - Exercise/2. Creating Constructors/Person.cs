39d17837f708746a7b0204ac2c48d0705a62473d
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-05-08 22:02:30 +0100

    Solutions

---
+++ Defining Classes - Exercise/2. Creating Constructors/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        //fields
+        private string name;
+        private int age;
+
+        //constructors
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age) : this(age)
+        {
+            this.Name = name;
+        }
+
+        //properties
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                //i can make validation
+                //if (value < 18)
+                //{
+                //    throw new ArgumentException("You are too young");
+                //}
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ Defining Classes - Exercise/2. Creating Constructors/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using DefiningClasses;
+using System;
+
+public class StartUp
+{
+    private static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Name = "Peter";
+        person.Age = 18;
+
+        
+        Console.WriteLine($"{person.Name} is {person.Age} years old");
+    }
+}
