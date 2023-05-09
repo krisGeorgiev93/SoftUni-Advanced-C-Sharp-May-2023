8d401b81f36ca0812d1660b7170e669aea9dc3e3
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-05-09 23:35:05 +0100

    Solutions

---
+++ Defining Classes - Exercise/9. Pokemon Trainer/Pokemon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._Pokemon_Trainer
+{
+    public class Pokemon
+    {
+        public Pokemon(string name,string element,int health) 
+        {
+            PokemonName = name;
+            Element = element;
+            Health = health;      
+        
+        }
+
+        public string PokemonName { get; set; }
+        public string Element { get; set; }
+        public int Health { get; set; }
+    }
+}
