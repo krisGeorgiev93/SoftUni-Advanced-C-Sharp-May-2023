0f06526c300006a3c58be13c913f362348b4a5b9
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-05-11 22:20:39 +0100

    Solutions

---
+++ Exam Preparation - Defining Classes/3. ClothesMagazine/Cloth.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Text;
+
+namespace ClothesMagazine
+{
+    public class Cloth
+    {
+        public Cloth(string color, int size, string type)
+        {
+            this.Color = color;
+            this.Size = size;
+            this.Type = type;
+        }
+
+        public string Color { get; set; }
+        public int Size { get; set; }
+        public string Type { get; set; }
+
+        public override string ToString()
+        {
+           StringBuilder sb = new StringBuilder();
+            sb.Append($"Product: {this.Type} with size {this.Size}, color {this.Color}");
+            return sb.ToString().Trim();
+        }
+
+    }
+}
---
+++ Exam Preparation - Defining Classes/3. ClothesMagazine/Magazine.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClothesMagazine
+{
+    public class Magazine
+    {
+        public Magazine(string type, int capacity)
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+
+        public List<Cloth> Clothes { get; set; } = new List<Cloth>();
+
+        public void AddCloth(Cloth cloth)
+        {
+            if (Clothes.Count < Capacity)
+            {
+                Clothes.Add(cloth);
+            }
+
+        }
+
+        public bool RemoveCloth(string color)
+        {
+            var cloth = Clothes.FirstOrDefault(x => x.Color == color);
+
+            if (cloth == null)
+            {
+                return false;
+            }
+            Clothes.Remove(cloth);
+            return true;
+        }
+
+        public Cloth GetSmallestCloth() => this.Clothes.OrderBy(c => c.Size).FirstOrDefault();
+
+        public Cloth GetCloth(string color) => this.Clothes.FirstOrDefault(c => c.Color == color);
+
+        public int GetClothCount() => this.Clothes.Count;
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Type} magazine contains:");
+
+            foreach (var cloth in this.Clothes.OrderBy(c => c.Size))
+            {
+                sb.AppendLine(cloth.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
