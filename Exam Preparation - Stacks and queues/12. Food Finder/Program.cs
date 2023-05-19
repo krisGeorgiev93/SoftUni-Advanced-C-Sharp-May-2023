51a2763fcb82084910ff2575bfcb4bff094194d1
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-05-19 23:35:42 +0100

    Solutions

---
+++ Exam Preparation - Stacks and queues/12. Food Finder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Food_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<char> vowels = new Queue<char>(Console.ReadLine().Split().Select(char.Parse));
+            Stack<char> consonants = new Stack<char>(Console.ReadLine().Split().Select(char.Parse));
+            Dictionary<string, HashSet<char>> words = new Dictionary<string, HashSet<char>>()
+            {
+                 { "pear", new HashSet<char>() },
+                { "flour",new HashSet<char>() },
+                { "pork",new HashSet<char>() },
+                { "olive", new HashSet<char>() },
+            };
+
+            while (consonants.Any())
+            {
+                char vowelLetter = vowels.Dequeue();
+                char consonantLetter = consonants.Pop();
+                foreach (var word in words)
+                {
+                    if (word.Key.Contains(vowelLetter))
+                    {
+                        word.Value.Add(vowelLetter);
+                    }
+                    if (word.Key.Contains(consonantLetter))
+                    {
+                        word.Value.Add(consonantLetter);
+                    }
+                }
+
+                vowels.Enqueue(vowelLetter);
+            }
+            // new dictionary with all foundWords --> if word.Value.Count == word.Key.Length ====> 1 found word
+            Dictionary<string, HashSet<char>> foundWords = new Dictionary<string, HashSet<char>>(words.Where(x => x.Value.Count == x.Key.Length));
+
+            Console.WriteLine($"Words found: {foundWords.Count()}{Environment.NewLine}{string.Join(Environment.NewLine, foundWords.Keys)}");
+        }
+    }
+}
