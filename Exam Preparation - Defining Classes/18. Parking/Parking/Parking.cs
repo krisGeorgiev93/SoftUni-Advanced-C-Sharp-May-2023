7e2fd350f384cd38f57c5ab5f999321d036f0fa0
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-06-05 22:26:39 +0100

    upload solutions

---
+++ Exam Preparation - Defining Classes/18. Parking/Parking/Parking.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        public Parking(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            data = new List<Car>();
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public List<Car> data { get; set; }
+
+
+        public void Add(Car car)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(car);
+            }
+        }
+        public bool Remove(string manufacturer, string model)
+        {
+            var targetCar = data.FirstOrDefault(x=> x.Manufacturer == manufacturer && x.Model == model);
+            if (targetCar != null)
+            {
+                data.Remove(targetCar);
+                return true;
+            }
+            return false;
+        }
+
+        public Car GetLatestCar() => data.OrderByDescending(x => x.Year).FirstOrDefault();
+
+        public Car GetCar(string manufacturer, string model) => data.FirstOrDefault(x=> x.Manufacturer == manufacturer && x.Model == model);
+
+        public int Count { get => data.Count; }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The cars are parked in {Type}:");
+            foreach (var car in data)
+            {
+                sb.AppendLine(car.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+    }
+}
